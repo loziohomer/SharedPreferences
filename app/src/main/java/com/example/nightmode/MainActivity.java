8adb4f3dad1bbeaf117b27344741f67ec08c287b
Omero Vitali <vitali.omero@gmail.com>
2022-05-16 17:59:51 +0200

    First commit

--- app/src/main/java/com/example/nightmode/MainActivity.java
+++ app/src/main/java/com/example/nightmode/MainActivity.java
@@ -9,7 +9,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
